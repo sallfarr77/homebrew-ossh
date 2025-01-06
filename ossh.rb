c88111b98c33fcb65c67d55f5ed5c0b3e635c18f
sallfarr77 <sallfarr@outlook.com>
2025-01-06 13:28:31 +0700

    edit ossh formula

--- ossh.rb
+++ ossh.rb
@@ -10,8 +10,8 @@ class Ossh < Formula
   depends_on "python@3.11"
 
   resource "rich" do
-    url "https://files.pythonhosted.org/packages/source/r/rich/rich-13.7.0.tar.gz"
-    sha256 "9882e171eb9aa2de604df589d81e6acbce08e5703eb9d1f3bb7237f975728c7f"
+    url "https://files.pythonhosted.org/packages/ab/3a/0316b28d0761c6734d6bc14e770d85506c986c85ffb239e688eeaab2c2bc/rich-13.9.4.tar.gz"
+    sha256 "439594978a49a09530cff7ebc4b5c7103ef57baf48d5ea3184f21d9a2befa098"
   end
 
   def install
